5170899270212c44442d5cf856abb4618289dd48
Juan Carlos Aguirre Arango <50622777+aguirrejuan@users.noreply.github.com>
2020-10-05 15:31:48 -0500

    update

--- classDeepSort.py
+++ classDeepSort.py
@@ -1,125 +1,175 @@
-import time, random
-import numpy as np
+import time 
 import cv2
-import matplotlib.pyplot as plt
+import numpy as np
 import tensorflow as tf
 from yolov3_tf2.models import (
     YoloV3, YoloV3Tiny
 )
 from yolov3_tf2.dataset import transform_images
-from yolov3_tf2.utils import draw_outputs, convert_boxes
-
-from deep_sort import preprocessing
-from deep_sort import nn_matching
-from deep_sort.detection import Detection
-from deep_sort.tracker import Tracker
-from tools import generate_detections as gdet
+from yolov3_tf2.utils import draw_YOLO, draw_DS
+import imutils
+from deepsort import deepsort_rbc
+import warnings
+warnings.filterwarnings('ignore')
 
 
-class DeepSort:
-    def __init__(self,max_cosine_distance = 0.5,
-                 nn_budget = None,nms_max_overlap = 1.0,
-                 model_filename = 'model_data/mars-small128.pb',num_classes=80,
-                 size = 416, classes = './data/labels/coco.names',
-                 Tiny = False ,
-                 weights = './weights/yolov3.tf',
-                 detector = False):
+# The following functions can be used to convert a value to a type compatible
+# with tf.Example.
+
+def _bytes_feature(value):
+  """Returns a bytes_list from a string / byte."""
+  if isinstance(value, type(tf.constant(0))):
+    value = value.numpy() # BytesList won't unpack a string from an EagerTensor.
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+def _float_feature(value):
+  """Returns a float_list from a float / double."""
+  return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
+def _int64_feature(value):
+  """Returns an int64_list from a bool / enum / int / uint."""
+  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+# tf.example definition with relevant features.
+def frame_example(raw, frame):
+
+  feature = {
+      'frame': _int64_feature(frame),
+      'raw_features': _bytes_feature(tf.io.serialize_tensor(raw)),
+  }
+
+  return tf.train.Example(features=tf.train.Features(feature=feature))
+
+
+class DeepSort: 
+    def __init__(self,num_classes=80,
+                 size = 416, classes = './data/coco.names',
+                 detectorTiny = False ,
+                 weights = './weights/yolov3.tf'):
+                 
         #constantes
-        self.max_cosine_distance = max_cosine_distance
-        self.nn_budget = nn_budget 
-        self.nms_max_overlap = nms_max_overlap 
-        self.model_filename = model_filename
-        self.encoder = gdet.create_box_encoder(self.model_filename, batch_size=1)
-        self.metric = nn_matching.NearestNeighborDistanceMetric("cosine", self.max_cosine_distance,self.nn_budget)
-        self.tracker = Tracker(self.metric)
         self.num_classes = num_classes
         self.size = size 
-        
+ 
         #parametros detector
         self.classes = classes
-        self.Tiny = Tiny 
-        self.detector  = detector
-        self.weights = weights 
+        self.detectorTiny = detectorTiny 
+        self.weights = weights         
+        self.deepsort = deepsort_rbc()
         
         self.physical_devices = tf.config.experimental.list_physical_devices('GPU')
         if len(self.physical_devices) > 0:
             tf.config.experimental.set_memory_growth(self.physical_devices[0], True)
             
-        if self.Tiny:
+        if self.detectorTiny:
             self.yolo = YoloV3Tiny(classes=self.num_classes)
         else:
             self.yolo = YoloV3(classes=self.num_classes)
         
         self.yolo.load_weights(self.weights).expect_partial()
 
-        self.class_names = [c.strip() for c in open(self.classes).readlines()]
-        
-           
-        
+        self.class_names = [c.strip() for c in open(self.classes).readlines()]        
+
     def __call__(self,img):
-        
+        """ parámetros: imagen 
+            Salida: boxes_ds, id_ds ,boxes, sco, classIDs, ids, scales, class_names
+             
+            boxes_ds y id_ds salidas del tracker 
+            boxes, sco, classIDs, ids, scales, class_names son salidas del detector
+            
+            
+            sco: score detecciones 
+            classIDs: entero que define a que clase pertenece la detección 
+            ids: entero que define el orden en que se realizaron las detecciones en cada frame 
+            scales: define cual salida de la yolo realizo la detección, recordar que se tienen tres salidas cada una define una escala 
+            class_names: es un vector de string que tiene los nombres de las clases 
+            """
+    
         img_in = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         img_in = tf.expand_dims(img_in, 0)
         img_in = transform_images(img_in, self.size)
+        ind, boxes, scores, classes, nums, feat = self.yolo(img_in)
         
-        boxes, scores, classes, nums = self.yolo.predict(img_in)
-        
-        # si solo queremos que funcione el detector 
-        if self.detector:
-            img = draw_outputs(img, (boxes, scores, classes, nums), self.class_names)
-            return img, boxes
-        
-        classes = classes[0]
-        names = []
-        for i in range(len(classes)):
-            names.append(self.class_names[int(classes[i])])
-            
-        names = np.array(names)
-        converted_boxes = convert_boxes(img, boxes[0])
-        features = self.encoder(img, converted_boxes)    
-        detections = [Detection(bbox, score, class_name, feature) for bbox, score, class_name, feature in zip(converted_boxes, scores[0], names, features)if class_name == "person"]
-        
-        #initialize color map
-        cmap = plt.get_cmap('tab20b')
-        colors = [cmap(i)[:3] for i in np.linspace(0, 1, 20)]
 
-        # run non-maxima suppresion
-        boxs = np.array([d.tlwh for d in detections])
-        scores = np.array([d.confidence for d in detections])
-        classes = np.array([d.class_name for d in detections])
-        indices = preprocessing.non_max_suppression(boxs, classes, self.nms_max_overlap, scores)
-        detections = [detections[i] for i in indices]        
+        bboxes_p = []
+        confidences = []
+        classIDs = []
+        feat_track = []
+        scales = []
+        ids = []
 
-        # Call the tracker
-        self.tracker.predict()
-        self.tracker.update(detections)
+        # feature pre-location
+        feat_all = []
+        conti = 0
 
-        for track in self.tracker.tracks:
-            if not track.is_confirmed() or track.time_since_update > 1:
-                continue
-            bbox = track.to_tlbr()
-            class_name = track.get_class()
-            color = colors[int(track.track_id) % len(colors)]
-            color = [i * 255 for i in color]
-            cv2.rectangle(img, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), color, 2)
-            cv2.rectangle(img, (int(bbox[0]), int(bbox[1]-30)), (int(bbox[0])+(len(class_name)+len(str(track.track_id)))*17, int(bbox[1])), color, -1)
-            cv2.putText(img, class_name+ "-" + str(track.track_id),(int(bbox[0]), int(bbox[1]-10)),0, 0.75, (255,255,255),2)
-            
+        for j, indi in enumerate(ind):
+            if indi is not None:
+                for i in indi:
+                    classID = int(classes[0][int(i[4])])
+                    if self.class_names[classID] == "person" or self.class_names[classID] == "person":
+                        sco = np.array(scores[0][int(i[4])])
+                        box_p = np.array(boxes[0][int(i[4])])
+                        # logging.info('\t{}, {}, {}'.format(class_names[classID],
+                        #                                    sco,box_p))
 
-        return img, [ list(np.append(int(track.track_id), track.to_tlwh()))  for track in self.tracker.tracks if track.is_confirmed() or not track.time_since_update > 1]
- 
-#%%   
+                        # Feature extraction
+                        x, y = np.array(i[1:3])
+                        feat_1 = feat[j][:, x, y, :][0]
+                        feat_track.append(feat_1)
+                        feat_all.append(np.concatenate([feat_1,
+                                              tf.expand_dims(classes[0][int(i[4])], 0), # object class
+                                              tf.expand_dims(i[4], 0)], axis=0)) # id object in frame
+
+                        # objects allocation
+                        ids.append(conti)
+                        conti += 1
+                        scales.append(j)
+                        confidences.append(float(sco))
+                        bboxes_p.append(box_p)
+                        classIDs.append(classID)
+
+
+        # ensure at least one detection exists
+        if bboxes_p:
+
+            # if cont == 46:
+            #     print(cont)
+            # feed deepsort with detections and features
+            tracker, detections_class = self.deepsort.run_deep_sort(img, np.asarray(bboxes_p),
+                                                              confidences,
+                                                              classIDs, scales,
+                                                              ids, feat_track)
+            classIDs_nms = detections_class[1]
+            scales_nms = detections_class[2]
+            ids_nms = detections_class[3]
+            # prelocation employed detections
+            boxes_nms = []
+            sco_nms = []
+            for det in detections_class[0]:
+                # Append NMS detection boxes
+                boxes_nms.append(det.to_tlbr())
+                sco_nms.append(det.confidence)
+
+            # prelocation of tracked boxes
+            boxes_ds = []
+            id_ds = []
+            for track in tracker.tracks:
+                if not track.is_confirmed() or track.time_since_update > 1:
+                    continue
+                #Append boxes and id.
+                boxes_ds.append(track.to_tlbr())   #Get the corrected/predicted bounding box
+                id_ds.append(str(track.track_id))  #Get the ID for the particular track.
+                
+        return  boxes_ds, id_ds ,boxes_nms, sco_nms, classIDs_nms, ids_nms, scales_nms, self.class_names   
+
+
 
 if __name__=="__main__":
-    tracker = DeepSort()
+
+    tracker = DeepSort()    
 
     vid = cv2.VideoCapture("./videos_test/TUD-Stadtmitte.avi")
 
-    width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = int(vid.get(cv2.CAP_PROP_FPS))
-    codec = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter('video.avi', codec, fps, (width, height))
     count = 0
     while True: 
         _, img = vid.read()
@@ -131,20 +181,17 @@ if __name__=="__main__":
                 continue
             else: 
                 break
-        img, tracks = tracker(img)
+	
+	
+        boxes_ds, id_ds ,boxes_nms, sco_nms, classIDs_nms, ids_nms, scales_nms, class_names  = tracker(img)    
+        img = draw_DS(img, boxes_ds, id_ds)    # para pintar el traker 
+        img = draw_YOLO(img, (boxes_nms, sco_nms, classIDs_nms, ids_nms, # para pintar el detector 
+                              scales_nms), class_names)
+                              
         cv2.imshow('output', img)
         if cv2.waitKey(1) == ord('q'):
             break
 
     vid.release()
-
     cv2.destroyAllWindows()
-
-
-
-
-
-
-
-
 
