95a47e03dba2ff0c33eac9dacfb91cf9c43da706
Juan Carlos Aguirre Arango <50622777+aguirrejuan@users.noreply.github.com>
2020-10-15 11:21:05 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -6,17 +6,20 @@ import numpy as np
 from classDeepSort import DeepSort
 from yolov3_tf2.utils import draw_YOLO, draw_DS
 import os 
+from datetime import datetime
+
 
 """
 Cosas por hacer:
 
--> actualizar la funciones draw_YOLO para que muestre clase y score (listo)
+-> actualizar la funciones draw_YOLO para que muestre clase y score 
 
 -> agregar boton de stop para cerrar los archivos a guardar 
 
-
-
 """
+
+		
+
 
 
 class App:
@@ -28,6 +31,9 @@ class App:
         #Ventana principal
         self.window = master
         self.window.title = "MAIN WINDOW"
+        #icono
+        photo = tk.PhotoImage(file = "./data/logo.png")
+        self.window.iconphoto(False, photo)
 
         # Menus
 
@@ -99,6 +105,7 @@ class App:
         self.photo = None
         self.caption = None
         self.fps = 25
+        self.frameindex = 0 
         self.filepath = os.path.join('data','girl.png')
         
         
@@ -108,19 +115,21 @@ class App:
         self.typeDetector = True #True si se quiere usar como detector           
         
         #variables para guardar 
-        self.saveAnnotations = None 
-        self.saveVideo = None 
-        
+        self.path_save_video = './outout/output.avi'
+        self.txt_track = open('./output/track-'+str(datetime.now())+'.txt', 'w')
+        self.txt_track.write('frame_index, id, min x, miny, max x, max y'+ '\n')
+        self.txt_detector = open('./output/detect-'+str(datetime.now())+'.txt', 'w')
+        self.txt_detector.write('min x, miny, max x, max y, score, ClassID'+ '\n')
         #variables mode 
         self.modeFunction = self.modeimage #por default 
         
-
         # Main
         self.update()
         self.window.mainloop()
         
-        #all the things tha you need to close put here 
-        
+        #all the things that you need to close put here 
+        self.txt_track.close()
+        self.txt_detector.close()
         # Solución problema de cierre de ventana 
         try:
             self.window.destroy()
@@ -137,12 +146,16 @@ class App:
         if self.MODE_VIDEO_REPRODUCE:
             self.modeFunction()
             self.trackdetec()
+            self.frameindex += 1
+            if self.modeFunction ==  self.modeimage:
+            	self.MODE_VIDEO_REPRODUCE = False 
 
-           
+
         self.photo = cv2.resize(self.frame, 
                                 dsize=(self.IMG_WIDTH, self.IMG_HEIGHT), 
                                 interpolation=cv2.INTER_AREA)
         self.photo = ImageTk.PhotoImage(image=Image.fromarray(self.photo))
+        
         self.CanvasMainImage.create_image(0, 0, image=self.photo, anchor=tk.NW)
         self.window.after(int(1000 * (1/self.fps)), self.update)
         
@@ -151,11 +164,15 @@ class App:
         """Actualiza frame con detecciones o tracks """
         if self.typeDetector or self.typeTracker: 
             boxes_ds, id_ds ,boxes_nms, sco_nms, classIDs_nms, ids_nms, scales_nms, class_names  = self.tracker(self.frame)
+            
         if self.typeTracker:    
-            self.frame = draw_DS(self.frame, boxes_ds, id_ds)    # para pintar el traker 
+            self.frame = draw_DS(self.frame, boxes_ds, id_ds)    # para pintar el traker
+            self.save_annotations_tracker(boxes_ds,id_ds) 
+        
         if self.typeDetector:
             self.frame = draw_YOLO(self.frame, (boxes_nms, sco_nms, classIDs_nms, ids_nms, # para pintar el detector 
                               scales_nms), class_names)
+            self.save_annotations_detector(boxes_nms, sco_nms, classIDs_nms)
     	
 
     def modeimage(self):
@@ -175,8 +192,6 @@ class App:
         else:
             self.MODE_VIDEO_REPRODUCE = False 
         	
-        
-
     def modestream(self):
         ret, self.frame = self.caption.read()
         self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
@@ -208,7 +223,6 @@ class App:
             self.modeFunction =  self.modeimage
             self.tracker.deepsort.reset_tracker()
         
-
     def filemenu_openv(self):
         """Open Video"""
         filepath = filedialog.askopenfilename(
@@ -224,6 +238,7 @@ class App:
             self.caption = cv2.VideoCapture(self.filepath)
             self.modeFunction =  self.modevideo
             self.tracker.deepsort.reset_tracker()
+            self.frameindex = 0
 
     def filemenu_opens(self):
         """Open Stream"""
@@ -231,9 +246,9 @@ class App:
         self.caption = cv2.VideoCapture(0)
         self.modeFunction = self.modestream
         self.tracker.deepsort.reset_tracker()
+        self.frameindex = 0
         
-        
-
+       
     # Botones
     def button_reproduce(self):
         self.MODE_VIDEO_REPRODUCE = True
@@ -247,9 +262,19 @@ class App:
         self.typeDetector = not self.typeDetector
     def toggleTracker(self):
         self.typeTracker = not self.typeTracker 
-    
-            
-
+        
+        
+    #functions to save files 
+    def save_annotations_tracker(self,boxes,ids):
+    	# min x, miny, max x, max y 
+    	for bbox, id_ in zip(boxes,ids):
+    		self.txt_track.write(str(self.frameindex)+','+str(id_)+','+str(bbox[0]) + ','+str(bbox[1]) + ','+str(bbox[2]) + ','+str(bbox[3]) + '\n') 
+    			
+    def save_annotations_detector(self,boxes, sco, classIDs):
+        # min x, miny, max x, max y 
+   	    for bbox, scor,classId in zip(boxes, sco, classIDs):
+   		    self.txt_detector.write(str(bbox[0]) + ','+str(bbox[1]) + ','+str(bbox[2]) + ','+str(bbox[3]) +','+ str(scor)+','+str(classId)+ '\n')
+   		
 
 if __name__=="__main__":
     root = tk.Tk()
