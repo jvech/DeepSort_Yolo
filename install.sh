b37ce9a18e8ce96fc71dcdab4e841d14a969bca8
jvech <jmvalenciae@unal.edu.co>
2020-11-02 16:08:40 -0500

    Trying to add an install manager

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+FILE_ID="1bt-qbLLqY97UfrWp99hlEIUFcKEgKvDX" 
+FILENAME="yolov3_weights.zip"
+
+echo "Downloading the weights\n"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${FILE_ID}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${FILE_ID}" -o ${FILENAME}
+rm ./cookie
+
+echo "Decompressing the weights\n"
+unzip -d ./data ${FILENAME}
+
+
+
